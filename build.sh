a0aecbaa17176edee9d6a9b9d82ae8312a64cdc2
Sachin Gavali <saching@udel.edu>
2021-08-02 16:11:38 -0700

    updated build script

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ export FORCE_CUDA=0
 export MAX_JOBS=8
 rm -r build
 python setup.py clean
-#python setup.py build -j 16
-#python setup.py bdist_wheel
+python setup.py build -j 16
+python setup.py bdist_wheel
 python setup.py install 
