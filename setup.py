e80e1decf47acd4967d98d10d774de8469795288
Sachin Gavali <saching@udel.edu>
2021-08-02 14:38:38 -0700

    initial setup.py done

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorch_random_walk",
+    version="0.0.1",
+    author="Sachin Gavali",
+    author_email="sachinx0e@gmail.com",
+    description="A pytorch extesion library to perform random walks on graph",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
