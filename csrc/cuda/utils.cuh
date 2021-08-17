9f80efa403d501e41624b50dcc50ba57fa522a9d
Sachin Gavali <saching@udel.edu>
2021-08-16 20:49:39 -0700

    Added CPU tensor check

--- csrc/cuda/utils.cuh
+++ csrc/cuda/utils.cuh
@@ -2,4 +2,5 @@
 
 #include <torch/extension.h>
 #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
+#define CHECK_CPU(x) TORCH_CHECK(x.is_cpu(), #x " must be a CPU tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x->is_contiguous(), #x " must be a contigous tensor")
