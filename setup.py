bca539f96111c4e4dae8337a7fcf540864556d82
Sachin Gavali <saching@udel.edu>
2021-10-18 23:15:34 +0000

    Build for 0.1.8 done

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ def get_extension():
 
 setuptools.setup(
     name="torch_rw",
-    version="0.1.7",
+    version="0.1.8",
     author="Sachin Gavali",
     author_email="sachinx0e@gmail.com",
     description="A pytorch extension library to perform random walks on graph",
